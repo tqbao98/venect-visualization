42e677d758ffcc3b30e5c0a8b3241f1f7527d5bc
Dat Huynh <pdathuynh@gmail.com>
2019-08-11 15:09:05 +0300

    init commit

---
+++ src/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="main-content">
+        <div className="title-row w-row">
+          <div className="logo-container w-col w-col-9">
+            <h3 className="heading">Energy cycle</h3>
+          </div>
+          <div className="w-col w-col-3" />
+        </div>
+      </div>
+    );
+  }
+}
