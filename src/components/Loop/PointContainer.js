65e556fbc7f5b334e9bb14b64df4e4b97f0eeb38
Dat Huynh <pdathuynh@gmail.com>
2019-09-03 07:36:30 -0700

    added display mapping

--- src/components/Loop/PointContainer.js
+++ src/components/Loop/PointContainer.js
@@ -5,32 +5,42 @@ const pointsList = [
   {
     "measurement" : "TE-001",
     "position" : {
-      "top" : 0,
-      "left" : 0
+      "top" : "0",
+      "left" : "0"
     },
   },
   {
     "measurement" : "TE-002",
     "position" : {
-      "top" : 30,
-      "left" : 0
+      "top" : "30",
+      "left" : "0"
     },
   },
   {
     "measurement" : "TE-003",
     "position" : {
-      "top" : 60,
-      "left" : 0
+      "top" : "60",
+      "left" : "0"
     },
   }
 ]
 
-export default function PointContainer (props) {
+const Container = (props) => {
+  const {position,measurement} = props.pointsList
   return (
-      <div style={{position:'absolute',top:0}}>
-        <div style={{textAlign:'center'}}>TE-001</div>
+      <div style={{position:'absolute',top:`${position.top}%`,left:`${position.left}%`}}>
+        <div style={{textAlign:'center'}}>{measurement}</div>
         <Chip size="small" label="Basic Chip"/>
       </div>
   );
 }
 
+export default function Points (props) {
+  const points = pointsList.map(e => (
+    <Container 
+      key={e.measurement}
+      pointsList={e} 
+    />
+  ))
+  return points
+}
